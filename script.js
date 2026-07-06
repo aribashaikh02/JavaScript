8f469039473a6a487524d13ef84f6a316d6f6181
ARIBA SHAIKH <shaikhariba0203@gmail.com>
2026-07-06 14:31:00 +0530

    javascript basics

---
+++ script.js
@@ -0,0 +1,28 @@
+//Javascript Basics
+
+//The code below prints a pyramid using console.log() in JavaScript.
+console.log("Pyramid using JavaScript Basics:");
+console.log("     #     ");
+console.log("    ###    ");
+console.log("   #####   ");
+console.log("  #######  ");
+console.log(" ######### ");
+console.log("###########");
+
+
+//The code below accesses the developer variable with its name in the console.log().
+console.log("\nAccessing the developer variable:");   
+let character = 'Hello';
+console.log(character);
+character = "World";
+console.log(character);
+
+
+//The code below accesses the secondCharacter variable with its name in the console.log().
+console.log("\nAccessing the secondCharacter variable:");
+let character = 'Hello';
+console.log(character);
+character = "World";
+let secondCharacter;
+secondCharacter = character;
+console.log(secondCharacter);
